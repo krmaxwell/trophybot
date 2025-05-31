eda5fbfb5d103a7ff3714975351869662e594b45
Kyle Maxwell <kylemxl@icloud.com>
2025-05-30 19:22:21 -0500

    Remove secrets so we can update them

--- deploy.py
+++ deploy.py
@@ -120,6 +120,8 @@ def deploy_cloud_run():
         "--region",
         GCP_REGION,
         "--allow-unauthenticated",
+        "--remove-secrets",
+        "DISCORD_TOKEN,DISCORD_PUBLIC_KEY",
         "--remove-env-vars",
         "DISCORD_TOKEN,DISCORD_PUBLIC_KEY",
         "--update-secrets",
